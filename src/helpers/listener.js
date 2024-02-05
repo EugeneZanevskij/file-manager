097f0c0f260b14d600ca24891c347c717b251fea
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-02-05 22:03:11 +0300

    feat: implement os --cpus functionality

--- src/helpers/listener.js
+++ src/helpers/listener.js
@@ -7,6 +7,7 @@ import { cp } from '../operations/cp.js';
 import { mv } from '../operations/mv.js';
 import { rm } from '../operations/rm.js';
 import { endOfApp } from './console.js';
+import { os } from '../operations/os.js';
 
 export const listener = async (data) => {
   const [command, path, newPath] = data.toString().trim().split(' ');
@@ -28,6 +29,8 @@ export const listener = async (data) => {
     case 'mv': await mv(path, newPath);
       break;
     case 'rm': await rm(path);
+      break;
+    case 'os': os(path);
       break;
     case '.exit': endOfApp();
       break;
--- src/operations/os.js
+++ src/operations/os.js
@@ -1,16 +1,23 @@
-import { EOL } from 'os';
-import { getWorkingDirectory } from '../helpers/console.js';
+import { EOL, cpus } from 'os';
+import { getCurrentDirectory } from '../helpers/console.js';
 
 export const os = (argument) => {
   if (argument) {
     switch (argument) {
       case '--EOL': 
         console.log(JSON.stringify(EOL));
-        getWorkingDirectory();
-        break
+        getCurrentDirectory();
+        break;
+      case '--cpus':
+        const cpusInfo = cpus().map(({ model, speed }) => {
+          return { model, clockRate: `${(speed / 1000).toFixed(2)} GHz` };
+        });
+        console.table(cpusInfo);
+        getCurrentDirectory();
+        break;
       default:
         console.log('Invalid input');
-        break
+        break;
     }
   } else {
     console.log('Operation failed');
