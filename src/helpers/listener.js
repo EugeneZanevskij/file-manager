64507b75bd5b027765868aa7e0760f11ceab24fa
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-02-05 21:00:13 +0300

    feat: implement cp, mv functionality

--- src/helpers/listener.js
+++ src/helpers/listener.js
@@ -1,8 +1,8 @@
 import {resolve, join} from 'path';
 import { chdir } from 'process';
 import { getCurrentDirectory } from './console.js';
-import { open, readdir, rename, stat } from 'fs/promises';
-import { createReadStream } from 'fs';
+import { open, readdir, rename, stat, unlink } from 'fs/promises';
+import { createReadStream, createWriteStream } from 'fs';
 import { pipeline } from 'stream/promises';
 import { output } from './output.js';
 
@@ -110,6 +110,39 @@ const rn = async (path, newPath) => {
   }
 }
 
+const cp = async (path, newPath) => {
+  try {
+    if (path && newPath) {
+      const currentDir = process.cwd();
+      const readStream = createReadStream(resolve(currentDir, path), {encoding: 'utf-8'});
+      const writeStream = createWriteStream(resolve(currentDir, newPath), {encoding: 'utf-8'});
+      await pipeline(readStream, writeStream);
+      getCurrentDirectory();
+    } else {
+      console.log('Invalid input');
+    }
+  } catch {
+    console.log('Operation failed');
+  }
+}
+
+const mv = async (path, newPath) => {
+  try {
+    if (path && newPath) {
+      const currentDir = process.cwd();
+      const readStream = createReadStream(resolve(currentDir, path), {encoding: 'utf-8'});
+      const writeStream = createWriteStream(resolve(currentDir, newPath), {encoding: 'utf-8'});
+      await pipeline(readStream, writeStream);
+      await unlink(resolve(currentDir, path));
+      getCurrentDirectory();
+    } else {
+      console.log('Invalid input');
+    }
+  } catch {
+    console.log('Operation failed');
+  }
+}
+
 export const listener = async (data) => {
   const [command, path, newPath] = data.toString().trim().split(' ');
   switch (command) {
@@ -124,6 +157,10 @@ export const listener = async (data) => {
     case 'add': await add(path);
       break;
     case 'rn': await rn(path, newPath);
+      break;
+    case 'cp': await cp(path, newPath);
+      break;
+    case 'mv': await mv(path, newPath);
       break;
     default: console.log(`Unknown command: ${command}`);
   }
