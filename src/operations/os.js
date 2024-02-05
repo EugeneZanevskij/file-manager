1aa8425a8fe16b50c6f480af305d39a7967974a1
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-02-05 22:09:22 +0300

    feat: implement os --homedir, --username, --architecture functionality

--- src/operations/os.js
+++ src/operations/os.js
@@ -1,4 +1,4 @@
-import { EOL, cpus } from 'os';
+import { EOL, cpus, homedir, userInfo, arch } from 'os';
 import { getCurrentDirectory } from '../helpers/console.js';
 
 export const os = (argument) => {
@@ -13,6 +13,18 @@ export const os = (argument) => {
           return { model, clockRate: `${(speed / 1000).toFixed(2)} GHz` };
         });
         console.table(cpusInfo);
+        getCurrentDirectory();
+        break;
+      case '--homedir':
+        console.log(`Home directory: ${homedir()}`);
+        getCurrentDirectory();
+        break;
+      case '--username':
+        console.log(`Username: ${userInfo().username}`);
+        getCurrentDirectory();
+        break;
+      case '--architecture':
+        console.log(`Architecture: ${arch()}`);
         getCurrentDirectory();
         break;
       default:
