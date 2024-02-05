bf0e4f5e75e9b88b8b3a151a71bfe314193265bc
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-02-05 18:08:18 +0300

    feat: create functions for directory

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 const userArgs = process.argv[2];
 const username = userArgs.startsWith('--username=') ? userArgs.replace('--username=', '') : 'Anonymous';
+import  { homedir } from 'os';
+import { chdir } from 'process';
 
 const startOfApp = () => {
   console.log(`Welcome to the File Manager, ${username}!`);
@@ -10,10 +12,21 @@ const endOfApp = () => {
   process.exit();
 }
 
+const getCurrentDirectory = () => {
+  console.log(`You are currently in ${process.cwd()}`);
+}
+
+const changeHomeDirectory = () => {
+  chdir(homedir());
+  getCurrentDirectory();
+}
+
 startOfApp();
+changeHomeDirectory();
 
 process.stdin.on('data', (data) => {
   process.stdout.write(data);
+  getCurrentDirectory();
 });
 
 process.on('SIGINT', () => {
