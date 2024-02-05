7e5cd3598801b134c9fc8fd11f7ce3f396458893
Eugene Zanevskij <zanevskijzenia@gmail.com>
2024-02-05 17:42:53 +0300

    feat: implement General (Application exits if user pressed ctrl+c or sent .exit command and proper message is printed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 const userArgs = process.argv[2];
 const username = userArgs.startsWith('--username=') ? userArgs.replace('--username=', '') : 'Anonymous';
 
-const startOfWork = () => {
+const startOfApp = () => {
   console.log(`Welcome to the File Manager, ${username}!`);
 };
 
-startOfWork();
+const endOfApp = () => {
+  console.log(`Thank you for using File Manager, ${username}, goodbye!`);
+  process.exit();
+}
+
+startOfApp();
+
+process.stdin.on('data', (data) => {
+  process.stdout.write(data);
+});
+
+process.on('SIGINT', () => {
+  endOfApp();
+})
